488113a9de2295b4bdda13b8145c7e7ab145366c
eJafari <ebrahimaria@gmail.com>
2024-06-12 05:33:19 -0700

    Update cookie.php

--- cookie.php
+++ cookie.php
@@ -9,26 +9,32 @@ setcookie($cookie_name, $cookie_value, $cookie_expire, "/");
 $remaining_time = $cookie_expire - time();
 ?>
 <div id="timer"></div>
-<script>
-    // Set the expiration time of the cookie in seconds
-    let expirationTime = <?php echo $remaining_time; ?>
+  <script>
+      // Set the expiration time of the cookie in seconds
+      let expirationTime = <?php echo $remaining_time; ?>
 
-    // Function to countdown and display the remaining time
-    function displayCountdownTime() {
-        let minutes = Math.floor(expirationTime / 60);
-        let seconds = expirationTime % 60;
+      // Function to countdown and display the remaining time
+      function displayCountdownTime() {
+          let minutes = Math.floor(expirationTime / 60);
+          let seconds = expirationTime % 60;
 
-        // Display the remaining time in minutes and seconds
-        document.getElementById('timer').innerHTML =  minutes +':'+ seconds ;
-        if (expirationTime > 0) {
-            expirationTime--;
-            setTimeout(displayCountdownTime, 1000); // Update every second
-        } else {
-            document.getElementById('timer').innerHTML =  'Cookie Expired' ;
+          // Display the remaining time in minutes and seconds
+          document.getElementById('timer').innerHTML =  minutes +':'+ seconds ;
+          if (expirationTime > 0) {
+              expirationTime--;
+              setTimeout(displayCountdownTime, 1000); // Update every second
+          } else {
+              document.getElementById('timer').innerHTML =  'Session Expired';
+              Swal.fire({
+                icon: "error",
+                title: 'Session Expired. Login again.',
+              }).then(function() {
+              // Redirect the user
+                window.location.href = "index.php";
+              });
+          }
+      }
 
-        }
-    }
-
-    // Call the function to start the countdown
-    displayCountdownTime();
-</script>
+      // Call the function to start the countdown
+      displayCountdownTime();
+  </script>
